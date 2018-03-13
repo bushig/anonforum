fbabc472b592c036527bbfe98999a9f8ad998268
bushig <azerot966@gmail.com>
2018-03-13 14:36:10 +0300

    Basic models

--- forum/models.py
+++ forum/models.py
@@ -4,4 +4,10 @@ from django.db import models
 
 class Post(models.Model):
 
-    def __init__(self):
+    text = models.CharField(max_length=600)
+    thread = models.ForeignKey(Thread, on_delete=models.CASCADE)
+    is_OP = models.BooleanField(default=False)
+
+class Thread(models.Model):
+    is_pinned = models.BooleanField(default=False)
+    is_archived = models.BooleanField(default=False)
