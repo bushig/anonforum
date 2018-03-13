4122e2b23bf5a6346b43cb2d92b99fb61b2bd1a4
bushig <azerot966@gmail.com>
2018-03-13 18:26:34 +0300

    Basic frontend config

--- anonforum/urls.py
+++ anonforum/urls.py
@@ -17,8 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path(r'', include('frontend.urls')),
     path('admin/', admin.site.urls),
-    path(r'api-auth/', include('rest_framework.urls'))
+    path(r'api-auth/', include('rest_framework.urls')),
 ]
 
 
--- forum/models.py
+++ forum/models.py
@@ -11,12 +11,20 @@ class Board(models.Model):
 
 
 class Thread(models.Model):
+    number = models.IntegerField()
     is_pinned = models.BooleanField(default=False)
     is_archived = models.BooleanField(default=False)
     board = models.ForeignKey(Board, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return "Thread №{}".format(self.number)
+
 
 class Post(models.Model):
+    number = models.IntegerField()
     text = models.CharField(max_length=600)
     thread = models.ForeignKey(Thread, on_delete=models.CASCADE)
     is_OP = models.BooleanField(default=False)
+
+    def __str__(self):
+        return "Post №{}".format(self.number)
