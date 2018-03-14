6f308ffd841326f8c2b6180d7123472a150432d9
bushig <azerot966@gmail.com>
2018-03-14 21:54:48 +0300

    Added basic serializers

--- forum/serializers.py
+++ forum/serializers.py
@@ -6,3 +6,13 @@ class BoardSerializer(serializers.ModelSerializer):
     class Meta:
         model = Board
         fields = ('name', )
+
+class ThreadSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Thread
+        fields = ('number', 'is_pinned', 'is_archived', 'created')
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ('number', 'text', 'is_OP', 'created')
