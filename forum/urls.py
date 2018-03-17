ee29343ba694c7f9aa557acbb91ea73dbcba97dd
bushig <azerot966@gmail.com>
2018-03-17 15:38:44 +0300

    Thread List view

--- forum/urls.py
+++ forum/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import IndexPage
+from .views import IndexPage, ThreadList
 
 urlpatterns = [
    path('', IndexPage.as_view()),
+   path('<str:board>/', ThreadList.as_view()),
 ]
--- forum/views.py
+++ forum/views.py
@@ -1,9 +1,24 @@
 from rest_framework.generics import ListAPIView, ListCreateAPIView
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, View
+from django.shortcuts import render
 
 from .models import Board, Thread,  Post
 from .serializers import BoardSerializer, PostSerializer, ThreadSerializer
 
 # Create your views here.
-class IndexPage(TemplateView):
-    template_name = 'index.html'
+class IndexPage(View):
+
+    def get(self, request, *args, **kwargs):
+        boards = Board.objects.all()
+        context = {'boards': boards}
+        return render(request, 'index.html', context)
+
+class ThreadList(View):
+    def get(self, request, *args, **kwargs):
+        board = kwargs.get('board')
+        if board:
+            threads = Thread.objects.filter(board__name=board, is_archived=False)
+            context = {'board': board, 'threads': threads}
+            return render(request, 'threads_list.html', context)
+        else:
+            pass #TODO: 404 page
